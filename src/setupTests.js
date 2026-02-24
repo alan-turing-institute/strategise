fe25747e57fead4ecf7d961fa9e2c71ddc1295e7
Ed Chalstrey <edwardchalstrey@gmail.com>
2026-02-24 15:51:47 +0000

    Stick to backend tests only

--- src/setupTests.js
+++
@@ -1,5 +0,0 @@
-import '@testing-library/jest-dom';
-import { vi } from 'vitest';
-
-// Mock for ResizeObserver, which can cause errors in JSDOM
-global.ResizeObserver = vi.fn().mockImplementation(() => ({ disconnect: vi.fn(), observe: vi.fn(), unobserve: vi.fn() }));
